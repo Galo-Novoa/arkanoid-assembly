c32c627810d16ab7b1b0fd7a781238716c21c8d4
Galo-Novoa <galocnovoa@gmail.com>
2025-11-05 12:00:12 -0300

    Debug eliminado.

--- src/main.asm
+++ src/main.asm
@@ -9,9 +9,6 @@ main:
     # Inicializar matriz de colisiones
     jal initCollisionMatrix
     
-    # DEBUG: Verificar que los bloques se registraron
-    jal debugCheckBlocks
-    
     # Registrar paleta inicial en matriz
     jal updatePaddleInMatrix
     
@@ -29,36 +26,6 @@ mainLoop:
     # ========== ACTUALIZAR PALETA EN MATRIZ ==========
     jal updatePaddleInMatrix
     
-    # ========== DEBUG: VERIFICAR POSICIÓN ACTUAL ==========
-    lw $a0, ballX
-    lw $a1, ballY
-    jal getObjectFromMatrix
-    
-    # Si hay algo en la posición actual, mostrar info
-    beqz $v0, no_debug
-    move $t9, $v0  # Guardar ID
-    
-    # Mostrar posición y objeto
-    li $v0, 1
-    lw $a0, ballX
-    syscall
-    li $v0, 11
-    li $a0, ','
-    syscall
-    li $v0, 1
-    lw $a0, ballY
-    syscall
-    li $v0, 11
-    li $a0, ':'
-    syscall
-    li $v0, 1
-    move $a0, $t9
-    syscall
-    li $v0, 11
-    li $a0, '\n'
-    syscall
-    
-no_debug:
     # ========== MOVER PELOTA Y VERIFICAR COLISIONES ==========
     jal moveBall
 
@@ -72,7 +39,7 @@ no_debug:
 
     # ========== DELAY ==========
     li $v0, 32
-    li $a0, 15  # Más lento para debugging
+    li $a0, 5
     syscall
 
     j mainLoop
